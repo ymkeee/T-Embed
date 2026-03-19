bc8e3dcee8817e136d5e69837a5002f168e3857e
ymkeee <forfunpaychilled@gmail.com>
2026-03-19 21:28:17 +0300

    Update image.ps1

--- image.ps1
+++ image.ps1
@@ -1,4 +1,4 @@
-# 1. hide console window immediately
+# 1. Hide console window immediately
 try {
     $c = '[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr h, int n);'
     $t = Add-Type -MemberDefinition $c -Name "W" -Namespace "Win" -PassThru
@@ -8,51 +8,38 @@ try {
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Add-Type -AssemblyName PresentationFramework,PresentationCore,WindowsBase
 
-# 2. settings
+# 2. Settings
 $u = 'https://github.com/ymkeee/T-Embed/raw/refs/heads/main/image.png'
 $f = "$env:TEMP\image.png"
 
 try {
-    # download image
+    # Download video
     Invoke-WebRequest -Uri $u -OutFile $f -ErrorAction Stop
 
-    # 3. create player ui with image and timer
+    # 3. Create Player UI
     $xaml = @"
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         WindowStyle="None" WindowState="Maximized" Topmost="True" 
-        Background="Black" Cursor="None" ShowInTaskbar="False"
-        Loaded="Window_Loaded">
-    <Grid>
-        <Image Name="img" Source="$f" Stretch="Uniform" />
-    </Grid>
+        Background="Black" Cursor="None" ShowInTaskbar="False">
+    <Grid><MediaElement Name="v" Source="$f" LoadedBehavior="Play" Stretch="Uniform" /></Grid>
 </Window>
 "@
 
     $reader = New-Object System.Xml.XmlNodeReader ([xml]$xaml)
     $w = [Windows.Markup.XamlReader]::Load($reader)
-    $img = $w.FindName("img")
+    $v = $w.FindName("v")
 
-    # add timer for 67 seconds
-    $timer = New-Object System.Windows.Threading.DispatcherTimer
-    $timer.Interval = [TimeSpan]::FromSeconds(67)
-    $timer.Add_Tick({
+    # Close window when video ends
+    $v.add_MediaEnded({
         $w.Close()
-        $timer.Stop()
     })
-    $timer.Start()
 
-    # event for loaded
-    $loadedEvent = {
-        $timer.Start()
-    }
-    $w.Add_Loaded($loadedEvent)
-
-    # show the window
+    # Show the window
     $w.ShowDialog() | Out-Null
 } 
 catch { exit }
 finally {
-    # 4. cleanup and self-destruct process
+    # 4. Cleanup and self-destruct process
     if (Test-Path $f) { Remove-Item $f -Force -ErrorAction SilentlyContinue }
     Stop-Process -Id $pid -Force
 }
